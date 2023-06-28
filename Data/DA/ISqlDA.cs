5d822af8b2dcefb37f33f25dfc99e2e3d9c54d8c
SphepheloN <SphepheloN@ZAZN-BET-0B725B>
2023-06-28 10:30:46 +0200

    Add project files.

---
+++ Data/DA/ISqlDA.cs
@@ -0,0 +1,13 @@
+﻿namespace Data.Layer.DA
+{
+    public interface ISqlDA
+    {
+        Task<IEnumerable<T>> GetData<T, P>
+            (string spName, P parameters,
+            string connectionId = "constr");
+
+        Task SaveData<T>
+            (string spName, T parameters,
+            string connectionId = "constr");
+    }
+}
---
+++ Data/DA/SqlDA.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+
+namespace Data.Layer.DA
+{
+    public class SqlDA : ISqlDA
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDA(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<IEnumerable<T>> GetData<T, P>
+            (string spName, P parameters,
+            string connectionId = "constr") //T is the type of return data & P is the type of parameter
+        {
+            using IDbConnection connection = new SqlConnection
+                (_config.GetConnectionString(connectionId));
+
+            return await connection.QueryAsync<T>
+                (spName, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task SaveData<T>
+            (string spName, T parameters,
+            string connectionId = "constr")
+        {
+            using IDbConnection connection = new SqlConnection
+                (_config.GetConnectionString(connectionId));
+
+            await connection.ExecuteAsync
+                (spName, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
